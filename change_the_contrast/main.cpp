8c1e9f05e456058fe9955917e55db5bd8a5c2f4b
dammmit777 <2775270442@qq.com>
2021-01-15 09:56:10 +0800

    add trackbar

--- change_the_contrast/main.cpp
+++ change_the_contrast/main.cpp
@@ -3,38 +3,46 @@
 
 using namespace cv;
 
-double alpha; //simple contrast control
-double beta;  //simple brightness control
+Mat img, new_img;
+int alpha = 0; //simple contrast control
+int beta = 0;  //simple brightness control
 
-int main(int argc, char **argv)
+void on_track(int, void *)
 {
-    Mat img = imread(argv[1]);
-    Mat new_img = Mat::zeros(img.size(), img.type());
-
-    std::cout << "Basic Linear Transforms" << std::endl;
-    std::cout << "-----------------------" << std::endl;
-    std::cout << "Enter the alpha value [1.0-3.0]:";
-    std::cin >> alpha;
-    std::cout << "Enter the beta value [0-100]:";
-    std::cin >> beta;
-
-    // for (int y = 0; y < img.rows; y++)
+    // for (int i = 0; i < img.rows; i++)
     // {
-    //     for (int x = 0; x < img.cols; x++)
+    //     for (int j = 0; j < img.cols; j++)
     //     {
     //         for (int c = 0; c < 3; c++)
     //         {
-    //             new_img.at<Vec3b>(y, x)[c] =
-    //                 saturate_cast<uchar>(alpha * (img.at<Vec3b>(y, x)[c]) + beta);
+    //             new_img.at<Vec3b>(i, j)[c] = saturate_cast<uchar>((double)alpha * (img.at<Vec3b>(i, j)[c]) + (double)beta);
     //         }
     //     }
     // }
-    img.convertTo(new_img, -1, alpha, beta);
+    img.convertTo(new_img, -1, (double)alpha, (double)beta);
+    imshow("New Image", new_img);
+}
+
+int main(int argc, char **argv)
+{
+    img = imread(argv[1]);
+    new_img = Mat::zeros(img.size(), img.type());
+
+    std::cout << "Basic Linear Transforms" << std::endl;
+    std::cout << "-----------------------" << std::endl;
+    // std::cout << "Enter the alpha value [1.0-3.0]:";
+    // std::cin >> alpha;
+    // std::cout << "Enter the beta value [0-100]:";
+    // std::cin >> beta;
+
     namedWindow("Original Image", WINDOW_AUTOSIZE);
     namedWindow("New Image", WINDOW_AUTOSIZE);
+    createTrackbar("alpha", "Original Image", &alpha, 3, on_track);
+    createTrackbar("beta", "Original Image", &beta, 100, on_track);
+    on_track(0, 0);
 
-    imshow("Origenal Image", img);
-    imshow("New Image", new_img);
+    imshow("Original Image", img);
+    
 
     waitKey(0);
 
